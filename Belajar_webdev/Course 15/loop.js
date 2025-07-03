e755842467331000a6a0a1c8943a5b555d2ca552
Figo Akbar <figoakbar15@gmail.com>
2025-07-03 16:40:24 +0700

    DOM

---
+++ Belajar_webdev/Course 15/loop.js
@@ -0,0 +1,8 @@
+var count = 99;
+
+while (count < 0){
+    console.log( count+" bottles of beer on the wall, "+count+" bottles of beer. Take one down and pass it around, "+count-1+ " bottles of beer on the wall.");
+    count--;
+}
+
+console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, "+count+" bottles of beer on the wall.");
---
+++ Belajar_webdev/Course 16/DOM Challenge Starting Files/index.js
@@ -0,0 +1,6 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+document.querySelector(".list a").style.color = "red"
+document.querySelector("button").style.backgroundColor = "yellow"
+document.querySelector("h1").classList.add("huge")
+document.querySelector("a").getAttribute("href")
+document.querySelector("a").setAttribute("href", "https://www.linkedin.com")
