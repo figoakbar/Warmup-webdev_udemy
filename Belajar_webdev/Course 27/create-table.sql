190a0ae1cbdb540f1e21541d03add73735884958
Figo Akbar <49114403+figoakbar@users.noreply.github.com>
2025-09-04 00:51:06 +0700

    basic auth

---
+++ Belajar_webdev/Course 27/create-table.sql
@@ -0,0 +1,38 @@
+CREATE TABLE student (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT
+);
+
+-- One to One --
+CREATE TABLE contact_detail (
+  id INTEGER REFERENCES student(id) UNIQUE,
+  tel TEXT,
+  address TEXT
+);
+
+CREATE TABLE homework_submission (
+  id SERIAL PRIMARY KEY,
+  mark INTEGER,
+  student_id INTEGER REFERENCES student(id)
+);
+
+CREATE TABLE class (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(45)
+);
+
+CREATE TABLE enrollment (
+  student_id INTEGER REFERENCES student(id),
+  class_id INTEGER REFERENCES class(id),
+  PRIMARY KEY (student_id, class_id)
+);
+
+INSERT INTO student (first_name, last_name)
+VALUES ('Jack', 'Bauer');
+
+INSERT INTO class (title)
+VALUES ('English Literature'), ('Maths'), ('Physics');
+
+INSERT INTO enrollment (student_id, class_id ) VALUES (1, 1), (1, 2);
+INSERT INTO enrollment (student_id ,class_id) VALUES (2, 2), (2, 3);
